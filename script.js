ca0e1896454399f3db84bfd8e3f5b01a4e79ea1e
sayushin <shinya.sayu@gmail.com>
2023-03-26 09:32:14 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const joke = document.querySelector('#joke')
+
+const jokeBtn = document.querySelector('#joke-btn')
+
+function getJoke(){
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET','https://api.chucknorris.io/jokes/random');
+    
+    xhr.onreadystatechange = 
+    function (){
+    if(this.readyState === 4 && this.status === 200){
+        const data = JSON.parse(this.responseText)
+        const jokeText =data.value
+        console.log(jokeText) 
+        joke.textContent=jokeText
+    }}
+
+    xhr.send()
+
+
+}
+
+
+getJoke()
+jokeBtn.addEventListener('click',getJoke)
